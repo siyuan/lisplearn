183fe59fff22d23fd3a1648770e084a1a2bfaf73
siyuan <siyuan@FreeLnx.(none)>
2014-07-24 17:52:31 +0800

    0724

--- cd.lisp
+++ cd.lisp
@@ -41,3 +41,15 @@
        (if ripped-p (equal (getf cd :ripped) ripped) t))))
 (defun select (selector-fn)
   (remove-if-not selector-fn *db*))
+(defun update (selector-fn &key title artist rating (ripped nil ripped-p))
+  (setf *db*
+	(mapcar
+	 #'(lambda (row)
+	     (when (funcall selector-fn row)
+	       (if title (setf (getf row :title) title))
+	       (if artist (setf (getf row :artist) artist))
+	       (if rating (setf (getf row :rating) rating))
+	       (if ripped-p (setf (getf row :ripped) ripped)))
+	     row) *db*)))
+(defun delete-rows (selector-fn)
+  (setf *db* (remove-if selector-fn *db*)))
