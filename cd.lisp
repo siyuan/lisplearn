932b4c5d5450c8ab2b8113549f828abb91127920
WANG Siyuan <wangsiyuanbuaa@gmail.com>
2014-07-25 17:11:17 +0800

    cd finished

--- cd.lisp
+++ cd.lisp
@@ -56,3 +56,10 @@
 ;Removing Duplication and Winning Big
 (defun make-comparison-expr (field value)
   (list 'equal (list 'getf 'cd field) value))
+(defun make-comparison-expr (field value)
+  `(equal (getf cd ,field) ,value))
+(defun make-comparisons-list (fields)
+  (loop while fields
+       collecting (make-comparison-expr (pop fields) (pop fields))))
+(defmacro where (&rest clauses)
+  `#'(lambda (cd) (and ,@(make-comparisons-list clauses))))
